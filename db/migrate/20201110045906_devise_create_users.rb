636737a5d14884b1fcdebd403ddde5a1b53f32a4
EC2 Default User <ec2-user@ip-172-31-66-254.ec2.internal>
2020-11-13 14:06:10 +0000

    [Update]マイグレーションファイルの修正。

--- db/migrate/20201110045906_devise_create_users.rb
+++ db/migrate/20201110045906_devise_create_users.rb
@@ -8,15 +8,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :encrypted_password, null: false, default: ""
 
       # 新規に追加
-      t.string :last_name, null: false, default: ""
-      t.string :first_name, null: false, default: ""
-      t.string :last_name_kana, null: false, default: ""
-      t.string :first_name_kana, null: false, default: ""
-      t.string :postcode, null: false, default: ""
-      t.string :address, null: false, default: ""
-      t.string :phone_number, null: false, default: ""
-      t.string :display_name, null: false, default: ""
-      t.text :discriotion, default: ""
+      t.string :last_name
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :postcode
+      t.string :address
+      t.string :phone_number
+      t.string :display_name
+      t.text :discription
       t.string :image_id, default: ""
       t.boolean :is_deleted, null: false, default: false
 
