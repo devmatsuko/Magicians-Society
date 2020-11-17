df845e9d74dbcd793f2ca4a5bc1998540d394b79
EC2 Default User <ec2-user@ip-172-31-66-254.ec2.internal>
2020-11-17 06:27:50 +0000

    [Add]ジャンルデータの作成。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#ジャンルデータの作成
+Genre.create!(
+  name: "ウォンド"
+)
+
+Genre.create!(
+  name: "四つ玉"
+)
+
+Genre.create!(
+  name: "ステージカード"
+)
+
+Genre.create!(
+  name: "ステージ小道具"
+)
+
+Genre.create!(
+  name: "クロースカード"
+)
+
+Genre.create!(
+  name: "コイン"
+)
+
+Genre.create!(
+  name: "その他"
+)
