c4686ba919495d7b6a005dffba7900a505660337
EC2 Default User <ec2-user@ip-172-31-37-187.ap-northeast-1.compute.internal>
2020-11-24 12:25:06 +0000

    [Add]検索機能の実装。

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root 'homes#top'
   # サイト紹介ページ
   get '/about' => 'homes#about', as: 'about'
+  # 検索機能実施
+  get '/search' => 'homes#search', as: 'search'
+  
 
   # ログイン関連(利用しない機能のルーティングは削除する)
   devise_for :users, skip: :all
@@ -64,8 +67,5 @@ Rails.application.routes.draw do
 
   # 販売関連
   resources :sales, only: [:update, :index, :show]
-
-  # 検索関連
-  get '/search' => 'searchs#search', as: 'search'
 
 end
