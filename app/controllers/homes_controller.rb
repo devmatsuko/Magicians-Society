5519554211452ca464565af7bfb589b9498f4185
EC2 Default User <ec2-user@ip-172-31-37-187.ap-northeast-1.compute.internal>
2020-11-24 07:15:23 +0000

    [Add]TOP画面の実装。

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,15 @@
 class HomesController < ApplicationController
     
   def top
+    # 退会していないユーザーのマジック投稿をランダムに4投稿取得
+    @magics = Magic.eager_load(:user).where(users: {is_deleted: false}).order("RANDOM()").limit(4)
+    # 退会していない全ユーザーの商品をランダムに4投稿取得
+    @products = Product.eager_load(:user).where(users: {is_deleted: false}).order("RANDOM()").limit(4)
+    
   end
   
   def about
   end
     
 end
+  
