e1b2458bb3eb6847cd6a66add5ac6f122c343f79
EC2 Default User <ec2-user@ip-172-31-37-187.ap-northeast-1.compute.internal>
2020-11-28 07:07:47 +0000

    [Update]ページャのデザイン。テストデータの追加。

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#ユーザーの作成
+# ユーザーの作成
 User.create!(
-
-  email: "a@a.com",
-  password: "matsuko",
-
+  email: "magician1@ms.com",
+  password: "M@gician",
   last_name: "小松",
   first_name: "真也",
   last_name_kana: "コマツ",
@@ -23,61 +21,50 @@ User.create!(
   description: "よろしくお願いします！",
   is_deleted: false
 )
-
 User.create!(
-
-  email: "b@b.com",
-  password: "matsuko",
-
-  last_name: "小松",
-  first_name: "真也",
-  last_name_kana: "コマツ",
-  first_name_kana: "シンヤ",
+  email: "magician2@ms.com",
+  password: "M@gician",
+  last_name: "中瀬",
+  first_name: "弘樹",
+  last_name_kana: "ナカセ",
+  first_name_kana: "ヒロキ",
   postcode: "0000000",
   address: "大阪府東大阪市1-11-11",
   phone_number: "00000000000",
-  display_name: "マジシャンツマコ",
+  display_name: "パフォーマーナカセ",
   description: "よろしくお願いします！",
   is_deleted: false
 )
-
 User.create!(
-
-  email: "c@c.com",
-  password: "matsuko",
-
-  last_name: "小松",
-  first_name: "真也",
-  last_name_kana: "コマツ",
-  first_name_kana: "シンヤ",
+  email: "magician3@ms.com",
+  password: "M@gician",
+  last_name: "杉山",
+  first_name: "良平",
+  last_name_kana: "スギヤマ",
+  first_name_kana: "リョウヘイ",
   postcode: "0000000",
   address: "大阪府寝屋川市1-11-11",
   phone_number: "00000000000",
-  display_name: "マジシャンコマツ",
+  display_name: "クリエイタースギ",
   description: "よろしくお願いします！",
   is_deleted: false
 )
-
-
 User.create!(
-
-  email: "d@d.com",
-  password: "matsuko",
-
-  last_name: "小松",
-  first_name: "真也",
-  last_name_kana: "コマツ",
-  first_name_kana: "シンヤ",
+  email: "magician4@ms.com",
+  password: "M@gician",
+  last_name: "中松",
+  first_name: "拓也",
+  last_name_kana: "ナカマツ",
+  first_name_kana: "タクヤ",
   postcode: "0000000",
   address: "大阪府八尾市1-11-11",
   phone_number: "00000000000",
-  display_name: "クリエイターマツコ",
+  display_name: "プロ観客中松",
   description: "よろしくお願いします！",
   is_deleted: false
 )
 
-
-#ジャンルデータの作成
+# ジャンルデータの作成
 Genre.create!(
   name: "カードマジック"
 )
